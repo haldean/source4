6e873c21f6f9cc2a2d638c38009bc18a88c0f0e1
Will Brown <will.h.brown@gmail.com>
2012-10-16 23:40:24 +1100

    God fucking damn it asdfaif

--- src/image.h
+++ src/image.h
@@ -1,23 +1,24 @@
 #ifndef __CRENDER_IMAGE_H__
 #define __CRENDER_IMAGE_H__
 
-#include <ImfArray.h>
-#include <ImfRgbaFile.h>
 #include "geometry.h"
+#include <png++/png.hpp>
 
-using namespace Imf;
+class Rgba {
+  public:
+    Rgba();
+    Rgba(float r, float g, float b, float a);
+    float r, g, b, a;
+};
 
 class Image {
   public:
     Image(int width, int height);
     void setPixel(int i, int j, const Color&);
-    void writeExr(const string& path) const;
-#ifdef WRITE_PNG
     void writePng(const string& path) const;
-#endif
 
   private:
-    Array2D<Rgba> pixels;
+    Rgba **pixels;
     int w, h;
 };
 
